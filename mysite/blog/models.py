438dede63a3693882320a5083f26306980c08c1c
Danzel Barber <danzelbarber@Danzels-MBP.attlocal.net>
2024-09-25 13:28:32 -0500

    committing changes to mysite

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,16 +1,25 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
+
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
-
-    PUBLISHED = "PB", "Published"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+        )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -20,6 +29,10 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
+    # model managers
+    objects = models.Manager()  # the default manager
+    published = PublishedManager() # The custom manager
+
     class Meta:
         ordering = ["-publish"]
         indexes = [
@@ -28,3 +41,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
